4a1832c6d7f0429bd2a2d4790c076bbaf6357b35
murraymilton <murray.l.milton@outlook.com>
2021-10-07 21:03:01 +0200

    Finalized console application for mortgage calculator

--- Mortgage Calculatort/src/com/company/Main.java
+++ Mortgage Calculatort/src/com/company/Main.java
@@ -1,11 +1,34 @@
 package com.company;
 
+import java.text.NumberFormat;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        int [][] numbers = {{3, 5, 7},{9,11,13}};
-        System.out.println(Arrays.deepToString(numbers));
+        //Use a constant here due the number not changing
+        final byte MONTHS_IN_YEARS = 12;
+        final byte PERCENT = 100;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the principal amount to be financed:");
+        int principal = scanner.nextInt();
+
+        System.out.print("Enter the annual interest rate:");
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest / PERCENT/MONTHS_IN_YEARS;
+
+        System.out.print("Enter the number of years for your loan:");
+        byte years = scanner.nextByte();
+        int numberOfPayments = years * MONTHS_IN_YEARS;
+
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1+ monthlyInterest,numberOfPayments) -1);
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Your Mortgage is: " + mortgageFormatted);
     }
 }
