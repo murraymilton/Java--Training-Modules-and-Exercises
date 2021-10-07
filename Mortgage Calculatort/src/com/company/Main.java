8c9581f0979890a839dc3d9615cbeb40dab2fa50
murraymilton <murray.l.milton@outlook.com>
2021-10-07 16:22:11 +0200

    Added a 2d array example, for testing print methods in Java

--- Mortgage Calculatort/src/com/company/Main.java
+++ Mortgage Calculatort/src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test");
+        int [][] numbers = {{3, 5, 7},{9,11,13}};
+        System.out.println(Arrays.deepToString(numbers));
     }
 }
