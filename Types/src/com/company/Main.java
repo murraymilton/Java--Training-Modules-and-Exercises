9bcacd65d8df922278e9022e79f1c51c43cb4cf6
murraymilton <murray.l.milton@outlook.com>
2021-10-12 10:42:38 +0200

    added module for primitive and reference type

---
+++ Types/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//Primitive Types
+        byte age = 30;
+        long viewsCount = 7_239_357_753L;
+        float price = 289.99F;
+        char firstLetter = 'M';
+        boolean canDrive = true;
+        
+    }
+}
