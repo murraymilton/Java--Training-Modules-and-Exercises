7b5845ad93ea0e2a865941ae55a85eb5de53ac75
murraymilton <murray.l.milton@outlook.com>
2021-10-13 03:00:29 +0200

    Added conditional exercise for fizzbuzz exercise

--- Control Flow/src/com/company/Main.java
+++ Control Flow/src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -11,7 +13,7 @@ public class Main {
 //        System.out.println(x <= y);
 //        System.out.println(x >= y);
 
-        // Logical Operators
+        // Logical Operator
 //        int temperature = 22 ;
 //        boolean isWarm = temperature > 20 && temperature < 30;
 //        System.out.println(isWarm);
@@ -37,8 +39,36 @@ public class Main {
 //        System.out.println("It's cold outside. You might need a jacket, unless your iceman");
 
         //Ternary Operator
-        int income = 120_000;
-        String className = income > 100_000 ? "First" : "Economy";
+//        int income = 120_000;
+//        String className = income > 100_000 ? "First" : "Economy";
+//
+//        // Switch Statement
+//
+//        String role = "admin";
+//
+//        switch(role){
+//            case "admin":
+//                System.out.println("Your admin rights have been enabled. ");
+//                break;
+//            case "user":
+//                System.out.println("Your user rights have been enabled.");
+//                break;
+//            default:
+//                System.out.println("Your guest account has been enabled with minimal access.");
+//        }
+        System.out.print("Enter a number:");
+        Scanner userInput =  new Scanner(System.in);
+        int number = userInput.nextInt();
+        if(number % 3 == 0 && number % 5 == 0){
+            System.out.println("FizzBuzz");
+        } else if(number % 3 == 0){
+            System.out.println("Buzz");
+        }else if(number % 5 == 0){
+            System.out.println("Fizz");
+        }else
+            System.out.println(number);
+
+
 
     }
 }
